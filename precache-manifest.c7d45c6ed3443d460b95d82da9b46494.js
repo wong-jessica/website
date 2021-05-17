bcc1b5a9ee5168f4d16645456ea9afc693ceede1
wong-jessica <jessica.97.wong@gmail.com>
2021-05-16 19:07:50 -0700

    Updates

---
+++ precache-manifest.c7d45c6ed3443d460b95d82da9b46494.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b092edeb3c7c0c640921",
+    "url": "/static/css/main.3f34ef5b.chunk.css"
+  },
+  {
+    "revision": "b092edeb3c7c0c640921",
+    "url": "/static/js/main.ade46595.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3e375216c16511b33b3e",
+    "url": "/static/js/2.2813fe80.chunk.js"
+  },
+  {
+    "revision": "f28936ac320da2ced749f4ba70474422",
+    "url": "/index.html"
+  }
+];
